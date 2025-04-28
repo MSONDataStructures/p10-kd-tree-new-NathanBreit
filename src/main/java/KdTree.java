45c02289aa1969714abdb1908eb0059b6d688941
NathanBreit <ngbreit@icloud.com>
2025-04-27 23:24:04 -0500

    insert and contains are gucci

--- src/main/java/KdTree.java
+++ src/main/java/KdTree.java
@@ -83,27 +83,29 @@ public class KdTree {
         // TODO: your code here
         if (p == null) throw new IllegalArgumentException();
 
+        // deals with issue of having root be null in the call
         if (root == null) {
             root = new Node();
             root.p = p;
             root.rect = new RectHV(0, 0, 1, 1);
             size++;
-        }
-        // used for insertion. carries current location (to be traversed if not null), Point
-        // (to be created into a Node and added once at null location), the rectangle
-        // corresponding to the current Node (will be updated every recursion
-        // by checking x-value of point if vertical or y-value of point if horizontal),
-        // and depth to determine whether split is vertical vs. horizontal
+        } else {
+            // used for insertion. carries current location (to be traversed if not null), Point
+            // (to be created into a Node and added once at null location), the rectangle
+            // corresponding to the current Node (will be updated every recursion
+            // by checking x-value of point if vertical or y-value of point if horizontal),
+            // and depth to determine whether split is vertical vs. horizontal
 
-        //* odd value for layer == vertical, even value == horizontal
-        root = insertHelper(root, p, root.rect, 1);
+            //* odd value for layer == vertical, even value == horizontal
+            root = insertHelper(root, p, 0, 0, 1, 1, 1);
+        }
     }
 
-    private Node insertHelper(Node current, Point2D p, RectHV rect, int layer) {
+    private Node insertHelper(Node current, Point2D p, double minX, double minY, double maxX, double maxY, int layer) {
         if (current == null) {
             Node newNode = new Node();
             newNode.p = p;
-            newNode.rect = rect;
+            newNode.rect = new RectHV(minX, minY, maxX, maxY);
             size++;
             return newNode;
         }
@@ -111,19 +113,34 @@ public class KdTree {
         // if vertical
         if (layer % 2 == 1) {
             // if go left
-                    // how do I change the rect values?
-            current.lb = insertHelper(current.lb, p, rect, layer + 1);
+            if (p.x() < current.p.x()) {
+
+                // recurse with new rectangle
+                current.lb = insertHelper(current.lb, p, minX, minY, current.p.x(), maxY, layer + 1);
+            }
             // if go right
-            current.rt = insertHelper(current.rt, p, rect, layer + 1);
+            if (p.x() >= current.p.x()) {
+
+                //recurse with new rectangle
+                current.rt = insertHelper(current.rt, p, current.p.x(), minY, maxX, maxY, layer + 1);
+            }
         }
 
         // if horizontal
         if (layer % 2 == 0) {
             // if go down
-            current.lb = insertHelper(current.lb, p, rect, layer + 1);
+            if (p.y() < current.p.y()) {
+
+                // recurse with new rectangle
+                current.lb = insertHelper(current.lb, p, minX, minY, maxX, current.p.y(), layer + 1);
+            }
             // if go up
-            current.rt = insertHelper(current.rt, p, rect, layer + 1);
+            if (p.y() >= current.p.y()) {
+                // recurse with new rectangle
+                current.rt = insertHelper(current.rt, p, minX, current.p.y(), maxX, maxY, layer + 1);
+            }
         }
+        return current;
     }
     /**
      * Returns true if the set contains point p.
@@ -133,9 +150,38 @@ public class KdTree {
      */
     public boolean contains(Point2D p) {
         // TODO: your code here
-        return false;
+        if (p == null) throw new IllegalArgumentException();
+        if (root == null) return false;
+
+        return containsHelper(root, p, 1);
     }
 
+    private boolean containsHelper(Node current, Point2D p, int layer) {
+        //fixed null pointer exception. It was an educated guess. yay me.
+        if (current == null) return false;
+
+        // what I like to call the if statement of success for recursion
+        if (p.equals(current.p)) {
+            return true;
+        }
+
+        // code is effectively the same as in insert method
+        if (layer % 2 == 1) {
+            if (p.x() < current.p.x()) {
+                return containsHelper(current.lb, p, layer + 1);
+            } else {
+                return containsHelper(current.rt, p, layer + 1);
+            }
+        }
+        if (layer % 2 == 0) {
+            if (p.y() < current.p.y()) {
+                return containsHelper(current.lb, p, layer + 1);
+            } else {
+                return containsHelper(current.rt, p, layer + 1);
+            }
+        }
+        return false;
+    }
     /**
      * Draws all points to standard draw.
      */
@@ -172,5 +218,24 @@ public class KdTree {
      * @param args the arguments
      */
     public static void main(String[] args) {
+        KdTree kdTree = new KdTree();
+        kdTree.insert(new Point2D(0.7, 0.2));
+        kdTree.insert(new Point2D(0.5, 0.4));
+        kdTree.insert(new Point2D(0.2, 0.3));
+        kdTree.insert(new Point2D(0.4, 0.7));
+        kdTree.insert(new Point2D(0.9, 0.6));
+
+        kdTree.Print(kdTree.root);
+        System.out.println(kdTree.contains(new Point2D(0.9, 0.6)));
+        System.out.println(kdTree.contains(new Point2D(0.5, 0.5)));
+        System.out.println(kdTree.contains(new Point2D(0.5, 0.4)));
+        System.out.println(kdTree.contains(new Point2D(0.7, 0.2)));
+    }
+
+    private void Print(Node node) {
+        if (node == null) return;
+        System.out.println(node.p);
+        Print(node.lb);
+        Print(node.rt);
     }
 }
